372263e2a8b6cb032af2b0c127e84786078052dc
Matthew <sanspariel.m@gmail.com>
2024-02-18 11:26:44 -0700

    Adds intro IntroScreen

---
+++ scenes/intro2.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.start()
+	
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _goup():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
