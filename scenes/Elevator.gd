6a0f9879d9aa6add1cd807803bc576070c53beb5
tina25n <nguyen.tina25@yahoo.com>
2024-02-18 08:48:36 -0800

    Fix merge and all the elevator stuff

--- scenes/Elevator.gd
+++ scenes/Elevator.gd
@@ -1,14 +1,11 @@
-extends Area2D
+extends CollisionShape2D
 
-var entered = false
+@onready var interaction_area = $InteractionArea
 
-func _on_body_entered(body: PhysicsBody2D):
-	entered = true
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interaction_area.interact = Callable(self, "_goup")
 
-func _on_body_exited(body):
-	entered = false
-
-func _process(delta):
-	if entered == true:
-		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene_to_file("res://scenes/level 2.tscn")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _goup():
+	get_tree().change_scene_to_file("res://scenes/level 2.tscn")
